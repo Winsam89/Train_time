d4c1e47b6470e891053ca2e6c7e51152ee100dc2
Winsam Roderos <Winsamhehe@Winsams-MacBook-Pro.local>
2019-04-02 20:33:23 -0700

    Update1

---
+++ app.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCfrbwcJGLLWIdaJS2hXy-FStVx-qaLYLg",
+    authDomain: "traintime-4a15a.firebaseapp.com",
+    databaseURL: "https://traintime-4a15a.firebaseio.com",
+    projectId: "traintime-4a15a",
+    storageBucket: "traintime-4a15a.appspot.com",
+    messagingSenderId: "158840931205"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
